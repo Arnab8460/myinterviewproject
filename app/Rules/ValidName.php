cf55f2db1d5c47708224568aecb29f90c5679477
Arnab Das <arnab0679@gmail.com>
2025-02-14 16:19:23 +0530

    this

--- app/Rules/ValidName.php
+++ app/Rules/ValidName.php
@@ -4,16 +4,29 @@ namespace App\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Rule;
 
-class ValidName implements ValidationRule
+class ValidName implements Rule
 {
-    /**
-     * Run the validation rule.
-     *
-     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
-     */
-    public function validate(string $attribute, mixed $value, Closure $fail): void
+    public function passes($attribute, $value)
     {
-        //
+        if (strlen($value) < 5 || strlen($value) > 100) {
+            return false;
+        }
+
+        if (!preg_match("/^[a-zA-Z][a-zA-Z-' ]{3,98}[a-zA-Z]$/", $value)) {
+            return false;
+        }
+
+        if (preg_match("/[\s'-]{2,}/", $value)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function message()
+    {
+        return 'The :attribute must contain only letters, spaces, hyphens, or apostrophes, without consecutive or trailing special characters, and be 5-100 characters long.';
     }
 }
